dbc5b2a24e43e602c9331000289e33233f9691ab
MerveTuncelCE <mervetuncel@windowslive.com>
2018-03-11 23:02:07 +0300

    used for testing

--- unimportant.php
+++ unimportant.php
@@ -3,7 +3,7 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <style>
-  .accordion {
+  .accordionm {
     background-color: #eee;
     color: #444;
     cursor: pointer;
@@ -16,7 +16,7 @@
     transition: 0.4s;
   }
 
-  .active, .accordion:hover {
+  .active, .accordionm:hover {
     background-color: #ccc;
   }
 
@@ -27,24 +27,81 @@
     overflow: hidden;
     transition: max-height 0.2s ease-out;
   }
+
+  .accordion1Header {
+    border: 1px solid #2F4F4F;
+    color: white;
+    background-color: #437C17;
+    font-family: Arial, Sans-Serif;
+    font-size: 12px;
+    font-weight: bold;
+    padding: 5px;
+    margin-top: 5px;
+    cursor: pointer;
+  }
+  .accordion1HeaderSelected {
+    border: 1px solid #2F4F4F;
+    color: white;
+    background-color: #4AA02C;
+    font-family: Arial, Sans-Serif;
+    font-size: 12px;
+    font-weight: bold;
+    padding: 5px;
+    margin-top: 5px;
+    cursor: pointer;
+  }
+  .accordion1Content {
+    background-color: #d1facc;
+    border: 1px dashed #2F4F4F;
+    border-top: none;
+    padding: 5px;
+    padding-top: 10px;
+  }
+  .accordionHeader {
+    border: 1px solid #2F4F4F;
+    color: white;
+    background-color: #2E4d7B;
+    font-family: Arial, Sans-Serif;
+    font-size: 12px;
+    font-weight: bold;
+    padding: 5px;
+    margin-top: 5px;
+    cursor: pointer;
+  }
+  .accordionHeaderSelected {
+    border: 1px solid #2F4F4F;
+    color: white;
+    background-color: #5078B3;
+    font-family: Arial, Sans-Serif;
+    font-size: 12px;
+    font-weight: bold;
+    padding: 5px;
+    margin-top: 5px;
+    cursor: pointer;
+  }
+  .accordionContent {
+    background-color: #D3DEEF;
+    border: 1px dashed #2F4F4F;
+    border-top: none;
+    padding: 5px;
+    padding-top: 10px;
+  }
+
 </style>
 </head>
 <body>
 
-  <h2>Animated Accordion</h2>
-  <p>Click on the buttons to open the collapsible content.</p>
-
-  <button class="accordion">Section 1</button>
+  <button class="accordionm">Section 1</button>
   <div class="panel">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
   </div>
 
-  <button class="accordion">Section 2</button>
+  <button class="accordionm">Section 2</button>
   <div class="panel">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
   </div>
-
-  <button class="accordion">Section 3</button>
+  <input type="hidden" >
+  <button class="accordionm">Section 3</button>
   <div class="panel">
     <table>
       <tbody>
@@ -95,7 +152,7 @@
   </div>
 
   <script>
-    var acc = document.getElementsByClassName("accordion");
+    var acc = document.getElementsByClassName("accordionm");
     var i;
 
     for (i = 0; i < acc.length; i++) {
