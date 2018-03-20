00bab371ba16c4708f6a4116791b6c5d1324e342
seymayigit <seymayigitbm.gmail.com>
2018-03-20 16:52:10 +0300

    dosya yeni eklendi

---
+++ personelPageSidebar.php
@@ -0,0 +1,25 @@
+<aside class="main-sidebar">
+      <!-- sidebar: style can be found in sidebar.less -->
+      <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+          <div class="pull-left image">
+            <img src="dist/img/avatar3.png" class="img-circle" alt="User Image">
+          </div>
+          <div class="pull-left info">
+            <br/><p>Merve Tunçel</p>
+          </div>
+        </div>
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+          <li class="header"></li>
+          <li>
+            <a href="index.php">
+              <i class="fa fa-book"></i> 
+              <span>Öğrenci Bilgileri</span>
+            </a>
+          </li>
+        </ul>
+      </section>
+      <!-- /.sidebar -->
+    </aside>
